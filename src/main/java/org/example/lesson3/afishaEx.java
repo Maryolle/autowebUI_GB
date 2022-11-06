bd6f9d547aeb88b1ab99d363330bb8fd5cd432d4
Maryolle <maryolle@yandex.ru>
2022-11-06 17:42:38 +0300

    -

--- src/main/java/org/example/lesson3/afishaEx.java
+++ src/main/java/org/example/lesson3/afishaEx.java
@@ -5,6 +5,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class afishaEx {
     public static void main(String[] args) throws InterruptedException {
@@ -16,6 +21,8 @@ public class afishaEx {
         WebElement inputSearch = webDriver.findElement(By.xpath("//input[@placeholder='Событие, актер, место']"));
         inputSearch.sendKeys("Брат");
         Thread.sleep(1000);
+
+        WebDriverWait webDriverWait = new WebDriverWait (webDriver, Duration.ofSeconds(5));
         webDriver.findElement(By.xpath("//div[.='Брат']")).click();
         Thread.sleep(5000);
 
---
+++ src/main/java/triangle/AreaOfTheTriangle.java
@@ -0,0 +1,20 @@
+package triangle;
+
+public class AreaOfTheTriangle{
+
+    public static void main( String[] args )
+    {
+        double a;
+
+        a = triangleArea(5, 7, 8);
+        System.out.println("A triangle with sides 3,3,3 has an area of:" + a);
+
+
+    }
+
+    public static double triangleArea( int a, int b, int c )
+    {
+        double pp = (a + b + c) / 2.0;  // полупериметр
+        return Math.sqrt(pp * (pp - a) * (pp - b) * (pp - c)); // формула Герона
+    }
+}
